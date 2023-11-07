3b77d117d7c668157afaac6c1562133a2d9033b2
Daub5ter <tsmaks2@gmail.com>
2023-11-07 20:41:37 +0300

    start adaptation-service

---
+++ adaptation-service/data/models.go
@@ -0,0 +1,21 @@
+package data
+
+import (
+	"database/sql"
+	"time"
+)
+
+const dbTimeout = time.Second * 3
+
+var db *sql.DB
+
+// New is the function used to create an instance of the data package. It returns the type
+// Model, which embeds all the types we want to be available to our application.
+func New(dbPool *sql.DB) Models {
+	db = dbPool
+
+	return Models{}
+}
+
+type Models struct {
+}
