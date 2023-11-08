a1f62ff4dec34f42263e3fa212a65fb4ab530270
Daub5ter <tsmaks2@gmail.com>
2023-11-08 13:20:39 +0300

    created new models function

--- adaptation-service/data/models.go
+++ adaptation-service/data/models.go
@@ -80,6 +80,89 @@ func (i *Instructions) GetAll() ([]*Instructions, error) {
 	return instructions, nil
 }
 
+func (i *Instructions) Insert(instruction Instructions) (int, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), dbTimeout)
+	defer cancel()
+
+	var newID int
+	stmt := `insert into instructions (title, description, created_at, updated_at) values ($1, $2, $3, $4) returning id`
+
+	err := db.QueryRowContext(ctx, stmt,
+		instruction.Title,
+		instruction.Description,
+		time.Now(),
+		time.Now(),
+	).Scan(&newID)
+
+	if err != nil {
+		return 0, err
+	}
+
+	return newID, nil
+}
+
+func (k *Instructions) GetOne(id int) (*Instructions, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), dbTimeout)
+	defer cancel()
+
+	query := `select id, title, description, created_at, updated_at from knowledges where id = $1`
+
+	var knowledge Instructions
+	row := db.QueryRowContext(ctx, query, id)
+
+	err := row.Scan(
+		&knowledge.ID,
+		&knowledge.Title,
+		&knowledge.Description,
+		&knowledge.CreatedAt,
+		&knowledge.UpdatedAt,
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &knowledge, nil
+}
+
+func (ui *UsersInstructions) Insert(usersKnowledges UsersInstructions) (int, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), dbTimeout)
+	defer cancel()
+
+	stmt := `insert into users_instructions (user_id, instruction_id) values ($1, $2, $3) returning user_id`
+
+	var userID int
+	err := db.QueryRowContext(ctx, stmt,
+		usersKnowledges.UserID,
+		usersKnowledges.InstructionID,
+		time.Now(),
+	).Scan(&userID)
+
+	if err != nil {
+		return 0, err
+	}
+
+	return userID, nil
+}
+
+func (ui *UsersInstructions) Update() error {
+	ctx, cancel := context.WithTimeout(context.Background(), dbTimeout)
+	defer cancel()
+
+	stmt := `update users_instructions set solved_at = $1
+		where user_id = $2 and instruction_id = $3`
+
+	_, err := db.ExecContext(ctx, stmt,
+		time.Now(),
+	)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
 func (ui *UsersInstructions) GetAll(id int) ([]*Instructions, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), dbTimeout)
 	defer cancel()
